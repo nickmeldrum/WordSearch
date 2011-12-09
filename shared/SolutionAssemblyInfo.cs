e8c9575900a8354d00b85b20a06a74958210ae33
Nick Meldrum <nick@nickmeldrum.com>
2011-12-09 23:48:29 +0000

    Added a global assemblyinfo file - so that solution wide assembly properties are in 1 place

---
+++ shared/SolutionAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("nickmeldrum.com")]
+[assembly: AssemblyProduct("WordSearcher")]
+[assembly: AssemblyCopyright("Copyright nickmeldrum.com © 2011")]
+
+// using semantic versioning, see http://semver.org
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
