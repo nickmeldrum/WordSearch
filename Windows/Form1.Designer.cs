028295a3f38c9d2cb614f2f32e21a6305ac85c55
Nick Meldrum <nick@nickmeldrum.com>
2011-12-13 10:21:14 +0000

    added a winforms project for visualising the search process - currently in transit to a painted solution for improved performance

---
+++ Windows/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Windows {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.foundWordsTextbox = new System.Windows.Forms.TextBox();
+            this.wordSearchPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.wordSearchPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(568, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(568, 41);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // foundWordsTextbox
+            // 
+            this.foundWordsTextbox.Location = new System.Drawing.Point(568, 70);
+            this.foundWordsTextbox.Multiline = true;
+            this.foundWordsTextbox.Name = "foundWordsTextbox";
+            this.foundWordsTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.foundWordsTextbox.Size = new System.Drawing.Size(130, 491);
+            this.foundWordsTextbox.TabIndex = 3;
+            // 
+            // wordSearchPictureBox
+            // 
+            this.wordSearchPictureBox.Location = new System.Drawing.Point(12, 14);
+            this.wordSearchPictureBox.Name = "wordSearchPictureBox";
+            this.wordSearchPictureBox.Size = new System.Drawing.Size(550, 550);
+            this.wordSearchPictureBox.TabIndex = 4;
+            this.wordSearchPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 574);
+            this.Controls.Add(this.wordSearchPictureBox);
+            this.Controls.Add(this.foundWordsTextbox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.searchButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word Search";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.wordSearchPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox foundWordsTextbox;
+        private System.Windows.Forms.PictureBox wordSearchPictureBox;
+    }
+}
+
---
+++ Windows/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Model;
+
+namespace Windows {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private WordSearchBox wordSearchBox;
+        private SearchEngine searchEngine;
+        private BackgroundWorker workerThread;
+
+        private Graphics canvas;
+        private Pen pen = new Pen(Color.Blue, 2F); 
+        private Brush brush = new SolidBrush(Color.Blue);
+        private Font font = new Font(FontFamily.GenericMonospace, 2);
+
+        private void Form1_Load(object sender, EventArgs e) {
+            workerThread = new BackgroundWorker();
+            workerThread.DoWork += workerThread_DoWork;
+            workerThread.RunWorkerCompleted += workerThread_RunWorkerCompleted;
+            workerThread.WorkerSupportsCancellation = true;
+
+            wordSearchBox = new WordSearchBox();
+            searchEngine = new SearchEngine(wordSearchBox);
+            searchEngine.BoxesBeingSearched += searchEngine_BoxesBeingSearched;
+            searchEngine.FoundWord += searchEngine_FoundWord;
+
+            DrawBox();
+        }
+
+        void workerThread_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            searchButton.Enabled = true;
+            cancelButton.Enabled = false;
+            RevertBoxColor();
+        }
+
+        void searchEngine_FoundWord(object sender, FoundWordEventsArgs e) {
+            Action appendFoundWordToTextBox = () => foundWordsTextbox.AppendText(e.Word + Environment.NewLine);
+            Invoke(appendFoundWordToTextBox);
+
+            foreach (var i in e.Boxes) {
+                lastBoxesSearched[i] = Color.DarkSeaGreen;
+                //searchBoxPanel.Controls[i].BackColor = Color.DarkSeaGreen;
+            }
+        }
+
+        private Dictionary<int, Color> lastBoxesSearched = new Dictionary<int, Color>();
+
+        void searchEngine_BoxesBeingSearched(object sender, BoxSearchingEventsArgs e) {
+            RevertBoxColor();
+            lastBoxesSearched = new Dictionary<int, Color>();
+
+            foreach (var i in e.Boxes) {
+                //lastBoxesSearched.Add(i, searchBoxPanel.Controls[i].BackColor);
+                //searchBoxPanel.Controls[i].BackColor = Color.Aqua;
+            }
+        }
+
+        private void RevertBoxColor() {
+            foreach (var box in lastBoxesSearched) {
+                //searchBoxPanel.Controls[box.Key].BackColor = box.Value;
+            }
+        }
+
+        private void DrawBox() {
+            //searchBoxPanel.Controls.Clear();
+            canvas = wordSearchPictureBox.CreateGraphics();
+            var letterWidth = wordSearchPictureBox.Width / wordSearchBox.BoxWidth;
+
+            for (var i = 0; i < wordSearchBox.Letters.Length; i++) {
+                var currentRow = i / wordSearchBox.BoxWidth;
+                var currentColumn = i % wordSearchBox.BoxWidth;
+
+                canvas.DrawRectangle(pen,
+                    letterWidth * currentColumn, letterWidth * currentRow,
+                    letterWidth, letterWidth);
+                canvas.DrawString(wordSearchBox.Letters[i].ToString(), font, brush, letterWidth * currentColumn, letterWidth * currentRow);
+
+                //var label = new Label {
+                //    Location = new Point(letterWidth * currentColumn, letterWidth * currentRow),
+                //    Size = new Size(letterWidth, letterWidth),
+                //    BorderStyle = BorderStyle.FixedSingle,
+                //    Text = wordSearchBox.Letters[i].ToString(),
+                //    BackColor = Color.Khaki
+                //};
+                //searchBoxPanel.Controls.Add(label);
+            }
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e) {
+            //for (var i = 0; i < searchBoxPanel.Controls.Count; i++) {
+            //    searchBoxPanel.Controls[i].BackColor = Color.Khaki;
+            //}
+
+            foundWordsTextbox.Clear();
+            cancelButton.Enabled = true;
+            searchButton.Enabled = false;
+
+            workerThread.RunWorkerAsync();
+        }
+
+        void workerThread_DoWork(object sender, DoWorkEventArgs e) {
+            searchEngine.Search();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+            searchEngine.Cancel = true;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e) {
+            searchEngine.Cancel = true;
+            searchButton.Enabled = true;
+            RevertBoxColor();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e) {
+            var letterWidth = wordSearchPictureBox.Width / wordSearchBox.BoxWidth;
+
+            for (var i = 0; i < wordSearchBox.Letters.Length; i++) {
+                var currentRow = i / wordSearchBox.BoxWidth;
+                var currentColumn = i % wordSearchBox.BoxWidth;
+
+                e.Graphics.DrawRectangle(pen,
+                    letterWidth * currentColumn, letterWidth * currentRow,
+                    letterWidth, letterWidth);
+                e.Graphics.DrawString(wordSearchBox.Letters[i].ToString(), font, brush, letterWidth * currentColumn, letterWidth * currentRow);
+
+                //var label = new Label {
+                //    Location = new Point(letterWidth * currentColumn, letterWidth * currentRow),
+                //    Size = new Size(letterWidth, letterWidth),
+                //    BorderStyle = BorderStyle.FixedSingle,
+                //    Text = wordSearchBox.Letters[i].ToString(),
+                //    BackColor = Color.Khaki
+                //};
+                //searchBoxPanel.Controls.Add(label);
+            }
+        }
+    }
+}
