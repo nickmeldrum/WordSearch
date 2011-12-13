b0efa4093deb8a4d7a646cb6682076c6efc1a151
Nick Meldrum <nick@nickmeldrum.com>
2011-12-13 10:19:32 +0000

    added events into searchengine - so users of class are notified when a block is searched and when a word is found added cancel property to allow for a search to be cancelled by a user of the class abstracted calculation of maxletters into a nested class so we are only doing it once fixed bug where searchright was only doing the 1st row added direction string so we know what direction a word was found added code for direction SearchDiagonallyRightDown removed delegate for maxletters - didn't need to be a delegate made extractword delegate return an array of letter indexes instead of the word - so the actual string creation only happens in 1 block of code
    changed testfixture to bind to the events to get visibility of what is going on and debug.write it out

--- Model/SearchEngine.cs
+++ Model/SearchEngine.cs
@@ -1,67 +1,180 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-using Model.Extensions;
 
 namespace Model {
-    public class SearchEngine {
-        private readonly int minimumLetters = 3;
+    public class BoxSearchingEventsArgs {
+        public string Direction { get; set; }
+        public List<int> Boxes { get; set; }
+    }
 
+    public class FoundWordEventsArgs {
+        public string Word { get; set; }
+        public string Direction { get; set; }
+        public List<int> Boxes { get; set; }
+    }
+
+    public delegate void BoxSearchingEventHandler(object sender, BoxSearchingEventsArgs e);
+    public delegate void FoundWordEventHandler(object sender, FoundWordEventsArgs e);
+
+    public class SearchEngine {
+        public event BoxSearchingEventHandler BoxesBeingSearched;
+        public event FoundWordEventHandler FoundWord;
+
+        private readonly int minimumLetters = 3;
         private readonly IList<string> foundWords = new List<string>();
+
+        public IList<string> FoundWords {
+            get { return foundWords; }
+        }
+
+        public bool Cancel { get; set; }
 
         private WordList wordList = new WordList();
         private WordSearchBox wordSearchBox = new WordSearchBox();
 
+        public SearchEngine(WordSearchBox wordSearchBox) {
+            this.wordSearchBox = wordSearchBox;
+        }
+
+        public SearchEngine() {
+        }
+
         public void Search() {
+            Cancel = false;
+
             for (var start = 0; start < wordSearchBox.Letters.Length; start++) {
-                // Search Right:
-                SearchFromLetter(start,
-                    () => wordSearchBox.BoxWidth - start,
-                    length => wordSearchBox.Letters.Substring(start, length));
+                var maxLetters = new MaxLetters(start, wordSearchBox.BoxWidth, wordSearchBox.BoxHeight);
 
-                // Search Diagonally Right Down:
-                // TODO!
+                SearchRight(start, maxLetters);
+                SearchDiagonallyRightDown(start, maxLetters);
+                SearchDown(start, maxLetters);
+                SearchDiagonallyLeftDown(start, maxLetters);
+                SearchLeft(start, maxLetters);
+                SearchDiagonallyLeftUp(start, maxLetters);
+                SearchUp(start, maxLetters);
+                SearchDiagonallyRightUp(start, maxLetters);
 
-                // Search Down:
-                var currentRow = start / wordSearchBox.BoxWidth;
-
-                SearchFromLetter(start,
-                    () => wordSearchBox.BoxHeight - currentRow,
-                    length => {
-                        var word = "";
-                        for (var charIndex = 0; charIndex < length; charIndex++) {
-                            word += wordSearchBox.Letters[start + (wordSearchBox.BoxWidth*charIndex)];
-                        }
-                        return word;
-                    });
-
-                // Search Diagonally Left Down:
-                // TODO!
-
-                // Search Left:
-                SearchFromLetter(start,
-                    () => start,
-                    length => wordSearchBox.Letters.Substring(start - length, length).Reverse());
-
-                // SearchDirection Diagonally Left Up:
-                // TODO!
-
-                // SearchDirection Up:
-                // TODO!
-
-                // SearchDirection Diagonally Right Up:
-                // TODO!
+                if (Cancel) break;
             }
         }
 
-        private void SearchFromLetter(int letterIndex, Func<int> calculateMaxLetters, Func<int, string> extractWord) {
-            var maxLetters = calculateMaxLetters();
+        private class MaxLetters {
+            public int Left { get; private set; }
+            public int Right { get; private set; }
+            public int Up { get; private set; }
+            public int Down { get; private set; }
+
+            public MaxLetters(int startLetter, int boxWidth, int boxHeight) {
+                var currentRow = startLetter / boxWidth;
+                var currentColumn = startLetter % boxWidth;
+
+                Left = currentColumn + 1;
+                Right = boxWidth - currentColumn;
+                Down = boxHeight - currentRow;
+                Up = currentRow + 1;
+            }
+        }
+
+        private void SearchLeft(int start, MaxLetters maxLetters) {
+            SearchFromLetter(maxLetters.Left,
+                length => {
+                    var a = new int[length];
+                    for (var i = 0; i < length; i++) {
+                        a[i] = start - i;
+                    }
+                    return a;
+                }, "left");
+        }
+
+        private void SearchRight(int start, MaxLetters maxLetters) {
+            SearchFromLetter(maxLetters.Right,
+                length => {
+                    var a = new int[length];
+                    for (var i = 0; i < length; i++) {
+                        a[i] = start + i;
+                    }
+                    return a;
+                }, "right");
+        }
+
+        private void SearchUp(int start, MaxLetters maxLetters) {
+            SearchFromLetter(maxLetters.Up,
+                length => {
+                    var a = new int[length];
+                    for (var i = 0; i < length; i++) {
+                        a[i] = start - (wordSearchBox.BoxWidth * i);
+                    }
+                    return a;
+                }, "up");
+        }
+
+        private void SearchDown(int start, MaxLetters maxLetters) {
+            SearchFromLetter(maxLetters.Down,
+                length => {
+                    var a = new int[length];
+                    for (var i = 0; i < length; i++) {
+                        a[i] = start + (wordSearchBox.BoxWidth * i);
+                    }
+                    return a;
+                }, "down");
+        }
+
+        private void SearchDiagonallyLeftDown(int start, MaxLetters maxLetters) {
+            //var diagonalMaxLetters = (maxLetters.Left < maxLetters.Down) ? maxLetters.Left : maxLetters.Down;
+
+            //SearchFromLetter(diagonalMaxLetters,
+            //    length => {
+            //        var a = new int[length];
+            //        for (var i = length - 1; i >= 0; i--) {
+            //            a[i] += start + (wordSearchBox.BoxWidth * i) + i;
+            //        }
+            //        return a;
+            //    }, "left and down");
+        }
+
+        private void SearchDiagonallyRightDown(int start, MaxLetters maxLetters) {
+            var diagonalMaxLetters = (maxLetters.Right < maxLetters.Down) ? maxLetters.Right : maxLetters.Down;
+
+            SearchFromLetter(diagonalMaxLetters,
+                length => {
+                    var a = new int[length];
+                    for (var i = length - 1; i >= 0; i--) {
+                        a[i] += start + (wordSearchBox.BoxWidth * i) + i;
+                    }
+                    return a;
+                }, "right and down");
+        }
+
+        private void SearchDiagonallyLeftUp(int start, MaxLetters maxLetters) {
+            // TODO
+        }
+
+        private void SearchDiagonallyRightUp(int start, MaxLetters maxLetters) {
+            // TODO
+        }
+
+        private void SearchFromLetter(int maxLetters, Func<int, int[]> calculateLetterBoxes, string direction) {
             if (maxLetters < minimumLetters) return;
 
             for (var wordLength = minimumLetters; wordLength < maxLetters + 1; wordLength++) {
-                var word = extractWord(wordLength);
+                if (Cancel) break;
 
-                if (wordList.IsInWordList(word))
-                    foundWords.Add(word);
+                var letterBoxes = calculateLetterBoxes(wordLength);
+
+                if (BoxesBeingSearched != null) {
+                    BoxesBeingSearched(this, new BoxSearchingEventsArgs { Boxes = letterBoxes.ToList(), Direction = direction });
+                }
+
+                var word = new string(letterBoxes.Select(index => wordSearchBox.Letters[index]).ToArray());
+
+                if (wordList.IsInWordList(word)) {
+                    FoundWords.Add(word);
+
+                    if (FoundWord != null) {
+                        FoundWord(this, new FoundWordEventsArgs { Boxes = letterBoxes.ToList(), Word = word, Direction = direction });
+                    }
+                }
             }
         }
     }
--- ModelTest/SearchEngineTestFixture.cs
+++ ModelTest/SearchEngineTestFixture.cs
@@ -1,15 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
 
 namespace Model.Test {
     [TestFixture]
     public class SearchEngineTestFixture {
 
         [Test]
-        public void TestSearchEngine()
-        {
+        public void TestSearchEngine() {
             var searchEngine = new SearchEngine();
-
+            searchEngine.BoxesBeingSearched += SearchEngineBoxesBeingSearched;
+            searchEngine.FoundWord += SearchEngineFoundWord;
             searchEngine.Search();
+
+            WriteOutFoundWords(searchEngine.FoundWords);
+        }
+
+        private static void SearchEngineFoundWord(object sender, FoundWordEventsArgs e) {
+            Debug.Write(string.Format("Found word: {0}, direction: {1} at: ", e.Word, e.Direction));
+            WriteBoxesOut(e.Boxes);
+        }
+
+        private static void SearchEngineBoxesBeingSearched(object sender, BoxSearchingEventsArgs e) {
+            Debug.Write(string.Format("Searching direction: {0}, ", e.Direction));
+            WriteBoxesOut(e.Boxes);
+        }
+
+        private static void WriteOutFoundWords(IEnumerable<string> foundWords) {
+            Debug.WriteLine("Found Words:");
+            foreach (var foundWord in foundWords) {
+                Debug.Write(foundWord + ", ");
+            }
+            Debug.Write(Environment.NewLine);
+        }
+
+        private static void WriteBoxesOut(IList<int> boxes) {
+            Debug.Write("[");
+            for (var i = 0; i < boxes.Count; i++) {
+                Debug.Write(boxes[i]);
+                if (i < boxes.Count - 1)
+                    Debug.Write(", ");
+            }
+
+            Debug.Write("]" + Environment.NewLine);
         }
     }
 }
