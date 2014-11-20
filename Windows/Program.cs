c43c2c8eb8ae8aa8c2e9bace362368dd777fa382
nickm-scribestar <nick.meldrum@scribestar.com>
2014-11-20 12:04:29 +0000

    cleaned up program class

--- Windows/Program.cs
+++ Windows/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Windows {
     static class Program {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main() {
             Application.EnableVisualStyles();
