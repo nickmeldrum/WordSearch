b0b958adbc0d644c5af6b3cceb5bfbf8771d9092
nickmeldrum <nick@nickmeldrum.com>
2014-12-01 23:42:24 +0000

    updated so boxes being searched are lit up

--- Windows/MainForm.cs
+++ Windows/MainForm.cs
@@ -24,11 +24,13 @@
 
         private Pen gridPen = new Pen(Color.IndianRed, 4F);
         private Brush foundWordBackgroundBrush = new SolidBrush(Color.Khaki);
+        private Brush searchIndexesBackgroundBrush = new SolidBrush(Color.SlateBlue);
         private Brush letterBrush = new SolidBrush(Color.Indigo);
 
         private Dictionary<int, Color> lastBoxesSearched = new Dictionary<int, Color>();
         private int letterWidth;
         private IList<int> indexesFound = new List<int>();
+        private IList<int> indexesBeingSearched = new List<int>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -49,36 +51,34 @@
         {
             searchButton.Enabled = true;
             cancelButton.Enabled = false;
+            indexesBeingSearched.Clear();
+            wordSearchPictureBox.Invalidate(false);
         }
 
         private void SearchEngineFoundWord(string direction, List<int> charIndexes, string word)
         {
+            foreach (var i in charIndexes.Where(i => !this.indexesFound.Contains(i)))
+                this.indexesFound.Add(i);
+
             Action appendFoundWordToTextBox = () => foundWordsTextbox.AppendText(word + Environment.NewLine);
             Invoke(appendFoundWordToTextBox);
 
-            foreach (var i in charIndexes.Where(i => !this.indexesFound.Contains(i)))
-            {
-                this.indexesFound.Add(i);
-            }
-
-            Action highlightFoundWordFunc = () => wordSearchPictureBox.Invalidate(false);
-            Invoke(highlightFoundWordFunc);
+            Action redraw = () => wordSearchPictureBox.Invalidate(false);
+            Invoke(redraw);
         }
 
         private void SearchEngineBoxesBeingSearched(string direction, List<int> charIndexes)
         {
-            lastBoxesSearched = new Dictionary<int, Color>();
+            indexesBeingSearched = charIndexes;
 
-            foreach (var i in charIndexes)
-            {
-                //lastBoxesSearched.Add(i, searchBoxPanel.Controls[i].BackColor);
-                //searchBoxPanel.Controls[i].BackColor = Color.Aqua;
-            }
+            Action redraw = () => wordSearchPictureBox.Invalidate(false);
+            Invoke(redraw);
         }
 
         private void searchButton_Click(object sender, EventArgs e)
         {
             foundWordsTextbox.Clear();
+            indexesBeingSearched.Clear();
             indexesFound.Clear();
             cancelButton.Enabled = true;
             searchButton.Enabled = false;
@@ -108,23 +108,20 @@
 
             for (var i = 0; i < this.searchEngineData.Letters.Length; i++)
             {
-                var currentRow = i / this.searchEngineData.Width;
-                var currentColumn = i % this.searchEngineData.Width;
+                var point = new Point(letterWidth * (i % this.searchEngineData.Width), letterWidth * (i / this.searchEngineData.Width));
+                var rect = new Rectangle(point, new Size(letterWidth, letterWidth));
 
-                if (indexesFound.Contains(i))
-                    e.Graphics.FillRectangle(foundWordBackgroundBrush,
-                        letterWidth * currentColumn, letterWidth * currentRow,
-                        letterWidth, letterWidth);
+                if (indexesFound.Contains(i) && !indexesBeingSearched.Contains(i))
+                    e.Graphics.FillRectangle(foundWordBackgroundBrush, rect);
 
-                e.Graphics.DrawRectangle(gridPen,
-                    letterWidth * currentColumn, letterWidth * currentRow,
-                    letterWidth, letterWidth);
+                if (indexesBeingSearched.Contains(i))
+                    e.Graphics.FillRectangle(searchIndexesBackgroundBrush, rect);
+
+                e.Graphics.DrawRectangle(gridPen, rect);
 
                 e.Graphics.DrawString(
                     this.searchEngineData.Letters[i].ToString(CultureInfo.InvariantCulture),
-                    font, letterBrush,
-                    (letterWidth * currentColumn), // + (letterWidth / 2),
-                    (letterWidth * currentRow)// + (letterWidth / 2)
+                    font, letterBrush, point
                 );
             }
         }
